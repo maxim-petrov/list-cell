2a832e88118ad35c490b70386f3ccf62b1bde2b9
Maxim Petrov <mail@maximpetrov.ru>
2025-03-24 11:28:59 +0300

    Fix checboxes

--- src/component/Component.jsx
+++ src/component/Component.jsx
@@ -187,8 +187,8 @@ const Component = ({
       role="checkbox"
       animate={getCustomTapAnimation()}
     >
-      <div className="list-cell-wrapper-1a8-2-2-1" style={{ position: 'relative', zIndex: 1 }}>
-        <div className="list-cell-leftSide-8c8-2-2-1" style={{ position: 'relative', zIndex: 1 }}>
+      <div className="list-cell-wrapper-1a8-2-2-1" style={{ position: 'relative' }}>
+        <div className="list-cell-leftSide-8c8-2-2-1" style={{ position: 'relative' }}>
           <div 
             className={`checkbox-root-09c-9-1-0 ${isSelected ? 'checkbox-checked-b61-9-1-0' : ''}`} 
             data-e2e-id={`${name}__label`}
@@ -196,7 +196,7 @@ const Component = ({
               e.stopPropagation();
               handleToggle();
             }}
-            style={{ position: 'relative', zIndex: 1 }}
+            style={{ position: 'relative' }}
           >
             <input 
               className="checkbox-input-688-9-1-0" 
@@ -208,34 +208,29 @@ const Component = ({
               checked={isSelected}
               onChange={handleCheckboxChange}
               onClick={(e) => e.stopPropagation()}
-              style={{ position: 'relative', zIndex: 1 }}
+              style={{ position: 'relative' }}
             />
-            <div className="checkbox-iconContainer-80d-9-1-0" style={{ position: 'relative', zIndex: 1 }}>
-              <motion.div 
-                className={`icon-root-864-6-0-3 checkbox-icon-044-9-1-0 list-cell-radio`} 
-                style={{ position: 'relative', zIndex: 1 }}
-                initial={radioAnimation.initial}
-                animate={{ scale: isSelected ? 1 : 0 }}
-                transition={customTokens ? getCustomRadioAnimation().transition : radioAnimation.transition}
+            <div className="checkbox-iconContainer-80d-9-1-0" style={{ position: 'relative' }}>
+              <div 
+                className={`icon-root-864-6-0-3 checkbox-icon-044-9-1-0 list-cell-radio ${isSelected ? 'checkbox-icon-visible' : ''}`}
+                style={{ position: 'relative' }}
               >
-                {isSelected && (
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" style={{ position: 'relative', zIndex: 1 }}>
-                    <path fill="currentColor" fillRule="evenodd" d="M14.015 4.092a.863.863 0 0 1-.018 1.202l-6.58 6.513a1.232 1.232 0 0 1-1.755-.014L1.994 8.049a.863.863 0 0 1 0-1.203.822.822 0 0 1 1.179 0l3.378 3.448 6.285-6.22a.822.822 0 0 1 1.179.018Z" clipRule="evenodd"></path>
-                  </svg>
-                )}
-              </motion.div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" style={{ position: 'relative' }}>
+                  <path fill="currentColor" fillRule="evenodd" d="M14.015 4.092a.863.863 0 0 1-.018 1.202l-6.58 6.513a1.232 1.232 0 0 1-1.755-.014L1.994 8.049a.863.863 0 0 1 0-1.203.822.822 0 0 1 1.179 0l3.378 3.448 6.285-6.22a.822.822 0 0 1 1.179.018Z" clipRule="evenodd"></path>
+                </svg>
+              </div>
             </div>
           </div>
         </div>
         
-        <div className="list-cell-title-64d-2-2-1" style={{ position: 'relative', zIndex: 1 }}>
+        <div className="list-cell-title-64d-2-2-1" style={{ position: 'relative' }}>
           <div className="list-cell-highlightMatchRoot-160-2-2-1">{title}</div>
           {subtitle && <div className="list-cell-subtitle-bb8-2-2-1">{subtitle}</div>}
         </div>
       </div>
       
       {imageSrc && (
-        <div className="list-cell-rightSide-e72-2-2-1" style={{ position: 'relative', zIndex: 1 }}>
+        <div className="list-cell-rightSide-e72-2-2-1" style={{ position: 'relative' }}>
           <motion.div 
             className="avtr-root-912-1-1-4 avtr-small-b73-1-1-4 avtr-circle-5ee-1-1-4 list-cell-avatar"
             animate={getCustomAvatarAnimation()}
